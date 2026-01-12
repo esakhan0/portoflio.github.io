8d6eb86f1201848e5af89553b0d07f50019ba982
Mr Esa Khan <channelkhan0@gmail.com>
2026-01-12 18:25:23 +0000

    Update project details in projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,10 +1,10 @@
 // data/projects.js
 window.PROJECTS = [
   {
-    title: "Portfolio Website",
-    date: "2026-08-11",
-    excerpt: "hi",
-    url: "projects/projectsproject-one.html",
+    title: "Making this Website using Dreamweaver!",
+    date: "2026-01-11",
+    excerpt: "In this project I made a fully functioning website! Click here to see more!",
+    url: "projects/project1",
     stack: ["HTML", "CSS", "JS"],
     source: "https://github.com/",
     demo: "#"
