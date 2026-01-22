97524627138301f188f1770fc2e5c46830616212
Mr Esa Khan <channelkhan0@gmail.com>
2026-01-22 20:19:07 +0000

    new projects

--- data/projects.js
+++ data/projects.js
@@ -1,10 +1,19 @@
 // data/projects.js
 window.PROJECTS = [
   {
-    title: "Portfolio Website",
-    date: "2026-08-11",
-    excerpt: "hi",
-    url: "projects/projectsproject-one.html",
+    title: "I built a portfolio using Dreamweaver",
+    date: "2026-01-22",
+    excerpt: "Click to learn more on how I created this very site!",
+    url: "projects/project1",
+    stack: ["HTML", "CSS", "JS"],
+    source: "https://github.com/",
+    demo: "#"
+  },
+  {
+    title: "I made a minecraft server that my friends can play on!",
+    date: "2026-01-25",
+    excerpt: "Click to see how and what I used to create my minecraft server!",
+    url: "projects/project2",
     stack: ["HTML", "CSS", "JS"],
     source: "https://github.com/",
     demo: "#"
